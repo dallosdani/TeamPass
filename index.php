3bd5427f190c20e8d0ec0cbfefac7735fe3e3447
Nils Laumaillé <nils@teampass.net>
2026-03-20 21:58:11 +0100

    Fix - manager access should be limited to heath system and some users views

--- index.php
+++ index.php
@@ -705,16 +705,21 @@ if ((null === $session->get('user-validite_pw') || empty($session->get('user-val
                                 <i class="fa-solid fa-database nav-icon"></i>
                                 <p>' . $lang->get('database') . '</p>
                                 </a>
-                            </li>
+                            </li>';
+    }
+    if ($session_user_admin === 1) {
+        echo '
                             <li class="nav-item">
-                                <a href="#" data-name="utilities.health" class="nav-link', $get['page'] === 'utilities.health' ? ' active' : '', '">
+                                <a href="#" data-name="utilities.health" class="nav-link'. $get['page'] === 'utilities.health' ? ' active' : ''. '">
                                 <i class="fa-solid fa-heart-pulse nav-icon"></i>
                                 <p>' . $lang->get('system_health') . '</p>
                                 </a>
-                            </li>
+                            </li>';
+    }
+    echo '
                         </ul>
                     </li>';
-    } ?>
+    ?>
                         </ul>
                     </nav>
                     <!-- /.sidebar-menu -->
